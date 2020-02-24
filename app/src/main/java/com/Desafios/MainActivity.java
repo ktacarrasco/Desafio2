22c2bd026e0b88b547b1f2fa8ff1c9885f2fa443
kta <kta.carrasco@gmail.com>
2020-02-24 13:20:35 -0300

    version1

--- app/src/main/java/com/Desafios/MainActivity.java
+++ app/src/main/java/com/Desafios/MainActivity.java
@@ -2,10 +2,14 @@ package com.Desafios;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
- //       ImageView miimagen = findViewById(R.id.miimagen);
-//        Picasso.get().load("https://sendeyo.com/up/d/6be1254bc4").into(miimagen);
+        ImageView miimagen = findViewById(R.id.miimagen);
+        Picasso.get().load("https://doc-images-adl.s3-sa-east-1.amazonaws.com/logo.svg").into(miimagen);
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -25,5 +29,18 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        Button button1 = findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intentExample();
+            }
+        });
+
+    }
+    private void intentExample(){
+        Intent intent = new Intent(this, MainRegistro.class);
+        startActivity(intent);
     }
 }
